ba5e41d30f38217f6b0ae1c550a523c476f967cf
R. Scott Shaffer <r_scott.shaffer@comcast.net>
2011-10-11 20:55:56 -0700

    fixed two errors

--- spec/exercise_spec.rb
+++ spec/exercise_spec.rb
@@ -17,8 +17,8 @@ describe "Strings" do
     
     it "should change the switch the case of each character" do
 
-      phrase = "The rain in Spain falls mainly on the plain"
-
+      # phrase = "The rain in Spain falls mainly on the plain"
+      phrase = "the rain in spain falls mainly on the plain"
       phrase.swapcase.should eq "THE RAIN IN SPAIN FALLS MAINLY ON THE PLAIN"
 
     end
@@ -29,12 +29,9 @@ end
 
 describe "Integer" do
   context "when calling odd" do
-
     it "should indicate if a given integer is odd" do
-
 	a_number = 4
-
-	a_number.odd?.should be true
+	a_number.odd?.should_not be true
     end
   end
 end
